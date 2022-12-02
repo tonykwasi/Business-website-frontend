a0e5a25ab165383623d92ecaaff64a6585d2324c
tonykwasi <godisgoodtome2007@gmail.com>
2022-12-02 18:40:58 +0000

    fifth commit

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Home.css";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
+
+
 
 export const Home = () => {
   return (
@@ -10,8 +11,13 @@ export const Home = () => {
 
       <Container fluid className="img-txt">
         
-          <p class="txt">Welcome To Company</p>
-        
+          <p class="txt1">Welcome To Company</p>
+          <p class="txt2">Lorem ipsum dolor sit amet</p>
+          <div class="hbtn">
+             <div ><Button variant="outline-info" >Read More</Button></div>
+             <div><Button variant="outline-info">About Us</Button></div>
+          </div>
+          
       </Container>
 
     </div>
